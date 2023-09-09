1fb4c4ac61a2894fcaae7789efa58ad4d3a3b2ba
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-09-09 10:26:01 -0300

    Update and rename Progama2.py to Progama2_TipoDeValor.py

--- PrimerosProgamas/Progama2_TipoDeValor.py
+++ PrimerosProgamas/Progama2_TipoDeValor.py
@@ -4,5 +4,5 @@ runtime = 1980
 
 # Define the rating_out_of_10 float variable below: 
 rating_out_of_10 = 7
-print(type(runtime))
-print(type(rating_out_of_10))
+print("tipo de valor Runtime: ",type(runtime))
+print("tipo de valor rating out od 10: ",type(rating_out_of_10))
