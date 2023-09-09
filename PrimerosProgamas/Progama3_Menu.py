bb74c0c62b783347c03a783a6d96ac4f54f60a8c
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-09-09 10:29:23 -0300

    Update and rename progama3.py to Progama3_Menu.py

--- PrimerosProgamas/Progama3_Menu.py
+++ PrimerosProgamas/Progama3_Menu.py
@@ -1,19 +1,19 @@
 # We've defined the variable "meal" here to the name of the food we ate for breakfast!
-meal = "Cafe y Pan"
+Comida = "Cafe y Pan"
 print("MENU")
 # Printing out breakfast
 print("Desayuno: ")
-print(meal)
+print(Comida)
 
 # Now update meal to be lunch!
-meal = "Bandeja Paisa :D"
+Comida = "Bandeja Paisa :D"
 
 # Printing out lunch
-print("Lunch:")
-print(meal)
+print("Almuerxo:")
+print(Comida)
 
 # Now update "meal" to be dinner
-meal = "arepa de huevo"
+Comida = "arepa de huevo"
 # Printing out dinner
-print("Dinner:")
-print(meal)
+print("Cena:")
+print(Comida)
