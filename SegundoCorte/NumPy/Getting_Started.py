ca7f5dafc5042b0a2745c7904335bfc59b4ea554
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 20:51:21 -0300

    Create Getting_Started.py

---
+++ SegundoCorte/NumPy/Getting_Started.py
@@ -0,0 +1,18 @@
+import numpy
+
+arr = numpy.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+#Ejemplo 2------------------------------------
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+#Chekeo
+import numpy as np
+
+print(np.__version__)
