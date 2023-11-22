71058d8234f19868ec1e3e191e8bc533c58dc44b
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 20:59:10 -0300

    Create Creatting_Arrays.py

---
+++ SegundoCorte/NumPy/Creatting_Arrays.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
+
+#Ejemplo 2---------------------------------------------
+import numpy as np
+
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
+
+#Ejemplo 3---------------------------------------------
+import numpy as np
+
+arr = np.array(42)
+
+print(arr)
+
+#Ejemplo 4---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+#Ejemplo 5---------------------------------------------
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr)
+
+#Ejemplo 6---------------------------------------------
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(arr)
+
+#Ejemplo 7---------------------------------------------
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+
+#Ejemplo 8---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print('number of dimensions :', arr.ndim)
+
+#Ejemplo 9---------------------------------------------
+
+#Ejemplo 10---------------------------------------------
+
+#Ejemplo 11--------------------------------------------
