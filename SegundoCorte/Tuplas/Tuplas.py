c5c5b5b62ac48ab659252d5bf214a54aa6de37c6
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 21:58:13 -0300

    Create Tuplas.py

---
+++ SegundoCorte/Tuplas/Tuplas.py
@@ -0,0 +1,9 @@
+def generar_tuplas_de_lista(lista):
+    
+    indice = 0
+    
+    while indice < len(lista)-1:
+        print(lista[indice],lista[indice+1])
+        indice = indice + 1
+        
+print(generar_tuplas_de_lista([64,68,97]))
