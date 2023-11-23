af636800025af72c907cbc425b840a0a1c83249d
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 21:17:17 -0300

    Create Array_Indexing.py

---
+++ SegundoCorte/NumPy/Array_Indexing.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[0])
+
+#Ejemplo 2------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[1])
+
+#Ejemplo 3------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+print(arr[2] + arr[3])
+
+#Ejemplo 4------------------------------------
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('2nd element on 1st row: ', arr[0, 1])
+
+#Ejemplo 5------------------------------------
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('5th element on 2nd row: ', arr[1, 4])
+
+#Ejemplo 6------------------------------------
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(arr[0, 1, 2])
+
+#Ejemplo 7------------------------------------
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('Last element from 2nd dim: ', arr[1, -1])
