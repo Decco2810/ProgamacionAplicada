cf70f2e42fc2343f4f3218d9a3273bc6d3048cdc
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 21:21:45 -0300

    Update Array_Slysing.py

--- SegundoCorte/NumPy/Array_Slysing.py
+++ SegundoCorte/NumPy/Array_Slysing.py
@@ -1,1 +1,61 @@
+import numpy as np
 
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5])
+
+#Ejemplo 2---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[4:])
+
+#Ejemplo 3---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[:4])
+
+#Ejemplo 4---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[-3:-1])
+
+#Ejemplo 5---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5:2])
+
+#Ejemplo 6---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[::2])
+
+#Ejemplo 7---------------------------------------------
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr[1, 1:4])
+
+#Ejemplo 8---------------------------------------------
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr[0:2, 2])
+
+#Ejemplo 9---------------------------------------------
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr[0:2, 1:4])
