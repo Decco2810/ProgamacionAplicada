37d8b516ab214f3ff3dfabb42a247f66498a471c
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 21:54:02 -0300

    Create Sorting_Arrays.py

---
+++ SegundoCorte/NumPy/Sorting_Arrays.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr))
+
+#Ejemplo 2------------------------------------
+import numpy as np
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr))
+
+#Ejemplo 3------------------------------------
+import numpy as np
+
+arr = np.array([True, False, True])
+
+print(np.sort(arr))
+
+#Ejemplo 4------------------------------------
+import numpy as np
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
