07b94f2d5966fb4beb5beeea3c36ff1dcb7072ab
Decco2810 <142340859+Decco2810@users.noreply.github.com>
2023-11-22 21:32:40 -0300

    Create Copy_vs_View.py

---
+++ SegundoCorte/NumPy/Copy_vs_View.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.copy()
+arr[0] = 42
+
+print(arr)
+print(x)
+
+#Ejemplo 2---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+arr[0] = 42
+
+print(arr)
+print(x)
+
+#Ejemplo 3---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+x[0] = 31
+
+print(arr)
+print(x)
+
+#Ejemplo 4---------------------------------------------
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+x = arr.copy()
+y = arr.view()
+
+print(x.base)
+print(y.base)
+
+
+#Ejemplo 5---------------------------------------------
+
+
+#Ejemplo 6---------------------------------------------
+
+
+#Ejemplo 7---------------------------------------------
+
+
+#Ejemplo 8---------------------------------------------
